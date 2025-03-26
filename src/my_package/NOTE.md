1393d3e1d7c7fc13b3413bae3458e506fad5c23e
JordenHuang <jorden90573@gmail.com>
2025-03-26 19:32:39 +0800

    Add my_package, with TurtleBot3Waffle inside
    my_package acts like the basic example of webots ROS2 tutorial.
    The difference is the robot is now TurtleBot3Waffle. Using both
    proto and URDF file for the robot model

---
+++ src/my_package/NOTE.md
@@ -0,0 +1,7 @@
+# NOTE
+
+## 2025/03/26 15:39
+
+ERROR: Missing declaration for 'MyCobot280Pi_solid', unknown node.
+ERROR: '/tmp/tmpn0459mil_world_with_URDF_robot.wbt':28:14: error: Skipped unknown 'MyCobot280Pi_solid' node or PROTO.
+WARNING: TurtleBot3Waffle "TurCobot" > DEF RIGHT_JOINT HingeJoint > Solid "right_wheel" > DEF RIGHT_WHEEL Pose > DEF WHEEL_SHAPE Group > Shape  > Mesh "": Unable to find resource at '/home/jordenhuang/webots_ros2/install/my_package/share/my_package/resource/protos/turtleBot3Waffle_mesh/wheel_shape.obj'.
---
+++ src/my_package/setup.py
@@ -0,0 +1,35 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'my_package'
+data_files = []
+data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
+data_files.append(('share/' + package_name + '/launch', ['launch/robot_launch.py']))
+data_files.append(('share/' + package_name + '/worlds', ['worlds/waffle.wbt']))
+data_files.append(('share/' + package_name + '/resource', ['resource/TurtleBot3Waffle.urdf']))
+data_files.append(('share/' + package_name + '/resource/protos', ['resource/protos/TurtleBot3Waffle.proto']))
+data_files.append(('share/' + package_name, ['package.xml']))
+
+# Include all mesh files
+mesh_files = glob('resource/protos/turtleBot3Waffle_mesh/*')
+data_files.append(('share/' + package_name + '/resource/protos/turtleBot3Waffle_mesh', mesh_files))
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=data_files,
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jordenhuang',
+    maintainer_email='jorden90573@gmail.com',
+    description='Webots and ROS2',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'my_robot_driver = my_package.my_robot_driver:main',
+        ],
+    },
+)
